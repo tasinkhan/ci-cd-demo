a714a9a5fb5bc4b8af3c993e96741af01d2c6868
tas1nkhan <tasin.khan@solshare.com>
2025-04-06 11:08:07 +0600

    Add subtraction functionality and corresponding tests

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 def add(a: int, b: int):
     return a + b
+
+def subtract(a: int, b: int):
+    return a - b
--- test_app.py
+++ test_app.py
@@ -1,7 +1,13 @@
-from app import add
+from app import add, subtract
 
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
